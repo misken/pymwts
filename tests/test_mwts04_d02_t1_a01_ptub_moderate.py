cae80e54cd4a3f9c239889d6ce2ab7c6f8c11e11
misken <isken@oakland.edu>
2019-03-27 21:23:57 -0400

    added tests

---
+++ tests/test_mwts04_d02_t1_a01_ptub_moderate.py
@@ -0,0 +1,10 @@
+import solvemwts
+solvemwts.solvemwts("mwts04_d02_t1_a01_ptub_moderate",
+                    "../tests/inputs/mwts04_d02_t1_a01_ptub_moderate_nous.dat",
+                    "../tests/outputs/",
+                    "cbc",
+                    1200.0,
+                    0.02,
+                    results_db="../tests/mwts04_d2456_cbc_testing.db")
+
+
